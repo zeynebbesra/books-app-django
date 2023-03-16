30906e41d5179160d3a3e5540f00262435c96ffa
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-03-16 14:17:31 +0300

    models are created

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static  # it allows us to connect our URL 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('home.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
