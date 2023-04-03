502fc28465101549e3eb44e2cb78743121474ad1
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-04-03 23:25:04 +0300

    admin screen

--- home/urls/user_urls.py
+++ home/urls/user_urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path('register/', views.registerUser, name="register"),
     path('profile/', views.getUserProfile, name="users-profile"),
     path('profile/update', views.updateUserProfile, name="users-profile-update"),
+
+    path('<str:pk>/', views.getUserById, name="user"),
+    path('update/<str:pk>/', views.updateUser, name="user-update"),
+
     path('', views.getUsers, name="users"),
+    path('delete/<str:pk>/', views.deleteUser, name="user-delete"),
+    
 ]
